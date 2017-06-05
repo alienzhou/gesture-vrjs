50904a033fb6552d2bb0a238251274fe103a1989
zhou hongxuan <zhouhongxuan@baidu.com>
2017-06-05 20:43:06 +0800

    语言类型； 修改首页； DeviceOrientation

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var app = express();
 app.use('/static', express.static('static'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    var html = '<h1>目录</h1>'
+                + '<div><a href="static/imgtest.html" target="_blank">实验</a></div>'
+                + '<div><a href="static/cube.html" target="_blank">CSS 3D</a></div>'
+                + '<div><a href="static/carousel.html" target="_blank">VR手势操控</a></div>'
+  res.send(html);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8085, function () {
   var host = server.address().address;
   var port = server.address().port;
 
